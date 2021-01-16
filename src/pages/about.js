25892cc2ad6ea93c9c6c055909be3ed5d201962d
broncs <jhowfrombrazil@gmail.com>
2021-01-16 13:08:07 -0300

    adding 404 page , and config dinamically title

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Head from "../components/head"
 
 const aboutPage = () => {
   return (
     <Layout>
+      <Head title="About" />
       <h1>About page</h1>
       <p>heelo my name is jhonatan</p>
       <p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-
+import Head from "../components/head"
 const IndexPage = () => {
   return (
     <Layout>
+      <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Jhonatan , a front-end developer , living in Guarulhos</h2>
       <p>
